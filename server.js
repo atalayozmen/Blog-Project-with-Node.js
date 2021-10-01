961752d9c21f47d52a080f8445ade60ce29590d4
atalayozmen <atalayozmen21@gmail.com>
2021-10-01 17:08:59 +0300

    mib

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Post = require("./models/post.js");
-mongoose.connect("mongodb://localhost/blogdb");
+mongoose.connect(process.env.MONGODB_URI);
 const methodOverride = require("method-override");
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express')
