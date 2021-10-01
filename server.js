7f53b922768f86302b43a7c6d6f2116fb76c4948
atalayozmen <atalayozmen21@gmail.com>
2021-10-01 16:59:22 +0300

    mib

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const postRouter = require("./routes/posts.js");
 const apiRouter = require("./routes/api.js");
 const app = express();
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
 
